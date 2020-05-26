18ef7d5dc867d6811d863ba90b0c81d789d69b21
Saturnino Abril <saturninoabril@saturninoabril.local>
2020-05-26 22:46:30 +0800

    updated per review

--- server/activate_hooks.go
+++ server/activate_hooks.go
@@ -1,43 +1,14 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/blang/semver/v4"
 	"github.com/pkg/errors"
 )
-
-const minimumServerVersion = "5.22.0"
-
-func (p *Plugin) checkServerVersion() error {
-	serverVersion, err := semver.Parse(p.API.GetServerVersion())
-	if err != nil {
-		return errors.Wrap(err, "failed to parse server version")
-	}
-
-	r := semver.MustParseRange(">=" + minimumServerVersion)
-	if !r(serverVersion) {
-		return fmt.Errorf("This plugin requires Mattermost v%s or later", minimumServerVersion)
-	}
-
-	return nil
-}
 
 // OnActivate is invoked when the plugin is activated.
 //
 // This demo implementation logs a message to the demo channel whenever the plugin is activated.
 // It also creates a demo bot account
 func (p *Plugin) OnActivate() error {
-	if err := p.checkServerVersion(); err != nil {
-		return err
-	}
-
-	if ok, err := p.checkRequiredServerConfiguration(); err != nil {
-		return errors.Wrap(err, "could not check required server configuration")
-	} else if !ok {
-		p.API.LogError("Server configuration is not compatible")
-	}
-
 	if err := p.IsValid(); err != nil {
 		return err
 	}
@@ -47,8 +18,4 @@ func (p *Plugin) OnActivate() error {
 	}
 
 	return nil
-}
-
-func (p *Plugin) checkRequiredServerConfiguration() (bool, error) {
-	return p.Helpers.CheckRequiredServerConfiguration(manifest.RequiredConfig)
 }
